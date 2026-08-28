820ff629157d62b91e2ed509f24fcdf435833e77
Andrei Tolentino <andreyytolentino@gmail.com>
2026-08-28 21:37:12 +0800

    Add portfolio files

---
+++ $example.js
@@ -0,0 +1,6 @@
+'Order total: $' + (5788 + 998 + 6786 + 679) /100
+'Estimated tax (10%): $' + (2042 + 799) / 100
+'Shipping & Handling: $' +  (2042 + 799) / 100
+'items (' + (2 + 2) + ') $' + (2042 + 799) / 100
+'Total before tax: $' + (5788 + 998) /100
+alert('Total before tax: $' + Math.round((5788 + 998) * 0.1) /100);
